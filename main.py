246cbe58735042a3b4b777fec0c541c2a7a3df53
Vedic Dutta <109514492+JLiebert73@users.noreply.github.com>
2023-12-02 02:23:35 +0530

    Create main.py
    refactoring for improved readability and efficiency

---
+++ main.py
@@ -0,0 +1,184 @@
+import os
+import pickle
+import numpy as np
+from tqdm.notebook import tqdm
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
+
+BASE_DIR = '/kaggle/input/flickr8k'
+WORKING_DIR = '/kaggle/working'
+
+vgg_model = VGG16()
+vgg_model = Model(inputs=vgg_model.inputs, outputs=vgg_model.layers[-2].output)
+
+features = {}
+image_directory = os.path.join(BASE_DIR, 'Images')
+
+for img_name in tqdm(os.listdir(image_directory)):
+    img_path = os.path.join(image_directory, img_name)
+    image = load_img(img_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    feature = vgg_model.predict(image, verbose=0)
+    image_id = img_name.split('.')[0]
+    features[image_id] = feature
+
+pickle.dump(features, open(os.path.join(WORKING_DIR, 'features.pkl'), 'wb'))
+
+with open(os.path.join(WORKING_DIR, 'features.pkl'), 'rb') as f:
+    features = pickle.load(f)
+
+with open(os.path.join(BASE_DIR, 'captions.txt'), 'r') as f:
+    next(f)
+    captions_doc = f.read()
+
+image_captions_mapping = {}
+for line in tqdm(captions_doc.split('\n')):
+    tokens = line.split(',')
+    if len(line) < 2:
+        continue
+    image_id, caption = tokens[0], tokens[1:]
+    image_id = image_id.split('.')[0]
+    caption = " ".join(caption)
+    if image_id not in image_captions_mapping:
+        image_captions_mapping[image_id] = []
+    image_captions_mapping[image_id].append(caption)
+
+def clean(mapping):
+    for key, captions in mapping.items():
+        for i in range(len(captions)):
+            caption = captions[i]
+            caption = caption.lower()
+            caption = caption.replace('[^A-Za-z]', '')
+            caption = caption.replace('\s+', ' ')
+            caption = 'startseq ' + " ".join([word for word in caption.split() if len(word)>1]) + ' endseq'
+            captions[i] = caption
+
+clean(image_captions_mapping)
+
+all_captions = []
+for key in image_captions_mapping:
+    for caption in image_captions_mapping[key]:
+        all_captions.append(caption)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(all_captions)
+vocab_size = len(tokenizer.word_index) + 1
+
+max_length = max(len(caption.split()) for caption in all_captions)
+
+image_ids = list(image_captions_mapping.keys())
+split = int(len(image_ids) * 0.90)
+train = image_ids[:split]
+test = image_ids[split:]
+
+def data_generator(data_keys, mapping, features, tokenizer, max_length, vocab_size, batch_size):
+    X1, X2, y = list(), list(), list()
+    n = 0
+    while 1:
+        for key in data_keys:
+            n += 1
+            captions = mapping[key]
+            for caption in captions:
+                seq = tokenizer.texts_to_sequences([caption])[0]
+                for i in range(1, len(seq)):
+                    in_seq, out_seq = seq[:i], seq[i]
+                    in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                    out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                    X1.append(features[key][0])
+                    X2.append(in_seq)
+                    y.append(out_seq)
+            if n == batch_size:
+                X1, X2, y = np.array(X1), np.array(X2), np.array(y)
+                yield [X1, X2], y
+                X1, X2, y = list(), list(), list()
+                n = 0
+
+inputs1 = Input(shape=(4096,))
+fe1 = Dropout(0.4)(inputs1)
+fe2 = Dense(256, activation='relu')(fe1)
+
+inputs2 = Input(shape=(max_length,))
+se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+se2 = Dropout(0.4)(se1)
+se3 = LSTM(256)(se2)
+
+decoder1 = add([fe2, se3])
+decoder2 = Dense(256, activation='relu')(decoder1)
+outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+plot_model(model, show_shapes=True)
+
+epochs = 20
+batch_size = 32
+steps = len(train) // batch_size
+
+for i in range(epochs):
+    generator = data_generator(train, image_captions_mapping, features, tokenizer, max_length, vocab_size, batch_size)
+    model.fit(generator, epochs=1, steps_per_epoch=steps, verbose=1)
+
+model.save(WORKING_DIR+'/best_model.h5')
+
+def idx_to_word(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+def predict_caption(model, image, tokenizer, max_length):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], max_length)
+        yhat = model.predict([image, sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = idx_to_word(yhat, tokenizer)
+        if word is None:
+            break
+        in_text += " " + word
+        if word == 'endseq':
+            break
+    return in_text
+
+actual, predicted = list(), list()
+
+for key in test:
+    captions = image_captions_mapping[key]
+    y_pred = predict_caption(model, features[key], tokenizer, max_length)
+    actual_captions = [caption.split() for caption in captions]
+    y_pred = y_pred.split()
+    actual.append(actual_captions)
+    predicted.append(y_pred)
+
+print("BLEU-1: %f" % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+print("BLEU-2: %f" % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def generate_caption(image_name):
+    image_id = image_name.split('.')[0]
+    img_path = os.path.join(BASE_DIR, "Images", image_name)
+    image = Image.open(img_path)
+    captions = image_captions_mapping[image_id]
+    print('---------------------Actual---------------------')
+    for caption in captions:
+        print(caption)
+    print('---------------------Predicted---------------------')
+    image = load_img(img_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    prediction = predict_caption(model, image, tokenizer, max_length)
+    print(prediction)
+
+generate_caption('3425418735_2f454486a5.jpg')
